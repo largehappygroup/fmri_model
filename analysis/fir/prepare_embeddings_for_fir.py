7e9f70998e753175f38c673680bdd9c70b85dcc5
birkenstonks <z.karas@vanderbilt.edu>
2025-09-30 01:17:20 -0500

    adding todos

--- analysis/fir/prepare_embeddings_for_fir.py
+++ analysis/fir/prepare_embeddings_for_fir.py
@@ -40,6 +40,8 @@ def make_delayed(stim, delays, circpad=False):
 #     pass
 
 
+# TODO - keep track of duplicated layers and condense that into one
+# save volume numbers to get rid of that data in fMRI files
 def organize_individual_layers(layers, keystroke_dict, embedding_dict):
 
     signal = { l : [] for l in layers }
@@ -115,10 +117,9 @@ def run_participants(model_path, model, task):
         
         
         # maybe TODO - reduce dimensionality of embeddings
+        # TODO - save the different layers
         
         break
-
-
 
 
 
