326a309b84d8367b2dd0e48992b8ca1d6551da71
Zach Karas <z.karas@vanderbilt.edu>
2026-03-12 19:47:49 +0000

    starting stats script

---
+++ analysis/fir/running_stats.py
@@ -0,0 +1,17 @@
+import os
+import re
+import numpy as np
+import nibabel as nib
+
+
+# Run stat tests based on nifti files of correlation coefficients
+
+
+# hemisphere t-test
+
+# need to match up by model - can do best models to reduce false positive rate
+
+# group statistics? 
+
+# will be by layer
+
