d0636feeef57db050967ddda938152abcc1ebf38
Zach Karas <z.karas@vanderbilt.edu>
2026-04-20 02:34:07 +0000

    layer plots

--- analysis/fir/set_up_pycortex.py
+++
@@ -1,6 +0,0 @@
-import cortex
-
-ref = "/home/zachkaras/atlases/MNI152_T1_2mm_brain.nii.gz"
-
-cortex.align.automatic_fsl(subject='fsaverage', xfmname='mni2py', reference=ref)
-# cortex.align.automatic(subject='fsaverage', xfmname='mni2py', reference=ref)
